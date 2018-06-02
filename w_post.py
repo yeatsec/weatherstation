5662cb08b23ae053c1eb0d3c318753091be80491
baileympearson <bailey.pearson@vanderbilt.edu>
2018-06-02 15:39:16 -0400

    Modified the w_pub file to use a the requests module to transfer the data.  Also added the w_post file, which contains a class to transfer the data to the server.

---
+++ w_post.py
@@ -0,0 +1,43 @@
+import requests
+import random
+
+class URLPost():
+	url = 'https://afternoon-garden-70836.herokuapp.com/updateWeather'
+	def __init__(self, temp=80, humidity=20,press=30,location='Nashville'):
+		self.temperature = temp
+		self.humidity = humidity
+		self.pressure = press
+		self.location = location
+		self.data = {'temperature' : self.temperature,
+				'humidity'	  : self.humidity,
+				'pressure'	: self.pressure,
+				'location' : self.location
+		}
+		self.r = None
+	
+	def getRandomTemp(self):
+		return random.randint(-10,110)
+
+	def getRandomHumidity(self):
+		return random.randint(0,101)
+
+	def getLocation(self):
+		r = requests.get('https://api.ipdata.co')
+		r = r.json()
+		return {'city':r['city'],
+				'country':r['country_name']	}
+
+	def randomizeData(self):
+		self.data = {'temperature' : getRandomTemp(),
+				'humidity'	  : getRandomHumidity(),
+				'location' : getLocation()
+		}
+	
+	def send(self):
+		self.r = requests.post( url=self.url,
+							data=self.data)
+	
+	def postStatusOk(self):
+		if self.r is not None:
+			return self.r.status_code == 200
+		return False
--- w_pub.py
+++ w_pub.py
@@ -3,6 +3,7 @@
 
 from sense_hat import SenseHat
 import zmq
+from w_post import URLPost
 
 # gather weather information
 sense = SenseHat()
@@ -13,8 +14,14 @@ press = sense.get_pressure()
 hum = sense.get_humidity()
 
 # publish weather information
+post = URLPost(temp,hum,press)
+post.send()
+
+
+'''
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.bind("tcp://*:5556")
 
 socket.send_string("%s %s %s\0" % (temp, press, hum))
+'''
